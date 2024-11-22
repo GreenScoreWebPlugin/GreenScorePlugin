715ba32873f44d30216945d644c9a132ef2727ea
Alex Moreno <alex.m@wikicampers.com>
2024-11-22 14:13:54 +0100

    Website Login and register

--- GreenScoreWebsite/tailwind.config.js
+++ GreenScoreWebsite/tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+      },
+      colors: {
+        'gs-green-950': '#233430',
+        'gs-green-800': '#2E4D49',
+        'gs-green-700': '#3B6A69',
+        'gs-green-600': '#4F8B8A',
+        'gs-green-500': '#5FBCBB',
+        'gs-green-400': '#7DD9D8',
+        'gs-green-300': '#A8E8E7',
+        'gs-green-200': '#C7F5F4',
+        'gs-green-100': '#E3FAFA',
+
+        'grey-950': '#030712',
+        'grey-700': '#424E62',
+        'grey-400': '#8595AB',
+        'grey-200': '#D5DBE2',
+      }
+    },
   },
   plugins: [],
 }
