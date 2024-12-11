deecbd45d5b89817456a2c566082f7a89065e128
Lucie <ldubos008@gmail.com>
2024-12-11 17:56:43 +0100

    content dashboard

--- GreenScoreWebsite/src/Controller/DashboardController.php
+++ GreenScoreWebsite/src/Controller/DashboardController.php
@@ -5,9 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DashboardController extends AbstractController
 {
+    private HttpClientInterface $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
     #[Route('/mon-organisation', name: 'app_mon_organisation')]
     public function monOrganisation(): Response
     {
@@ -29,9 +37,26 @@ class DashboardController extends AbstractController
     #[Route('/dernier-site-web-consulte', name: 'app_dernier_site_web_consulte')]
     public function siteWebSurveille(): Response
     {
+        $countryName = 'France'; // Le pays que vous voulez afficher
+        $flagUrl = null;
+        $error = null;
+
+        try {
+            $response = $this->httpClient->request('GET', 'https://restcountries.com/v3.1/name/' . strtolower($countryName));
+            $data = $response->toArray();
+
+            // Récupère le lien du drapeau
+            $flagUrl = $data[0]['flags']['svg'] ?? null;
+        } catch (\Exception $e) {
+            $error = 'Impossible de récupérer les informations pour ce pays.';
+        }
+
         return $this->render('dashboards/index.html.twig', [
-            'page' => 'dernier-site-web-consulte', 
-            'title' => 'Dernier site web consulté'
+            'page' => 'dernier-site-web-consulte',
+            'title' => 'Dernier site web consulté',
+            'country' => $countryName,
+            'flagUrl' => $flagUrl,
+            'error' => $error,
         ]);
     }
 }
