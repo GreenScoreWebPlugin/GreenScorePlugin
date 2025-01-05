ad8ac3aa9f26ffe094db4cf323c0a4cb228b8fa5
Lucie <ldubos008@gmail.com>
2025-01-05 01:16:41 +0100

    edit migration

--- GreenScoreWebsite/migrations/Version20250105000359.php
+++ GreenScoreWebsite/migrations/Version20250105000359.php
@@ -20,7 +20,7 @@ final class Version20250105000359 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE monitored_website (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, url_domain VARCHAR(255) DEFAULT NULL, url_full LONGTEXT DEFAULT NULL, queries_quantity INT DEFAULT NULL, carbon_footprint DOUBLE PRECISION DEFAULT NULL, data_transferred DOUBLE PRECISION DEFAULT NULL, resources LONGTEXT DEFAULT NULL, loading_time DOUBLE PRECISION DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, creation_date DATETIME NOT NULL, INDEX IDX_7458B0D5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE monitored_website (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, url_domain VARCHAR(255) DEFAULT NULL, url_full LONGTEXT DEFAULT NULL, queries_quantity INT DEFAULT NULL, carbon_footprint DOUBLE PRECISION DEFAULT NULL, data_transferred DOUBLE PRECISION DEFAULT NULL, resources LONGTEXT DEFAULT NULL, loading_time DOUBLE PRECISION DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, creation_date DATETIME NOT NULL DEFAULT current_timestamp(), INDEX IDX_7458B0D5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE monitored_website ADD CONSTRAINT FK_7458B0D5A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
     }
 
