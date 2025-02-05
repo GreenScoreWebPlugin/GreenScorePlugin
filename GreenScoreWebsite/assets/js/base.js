4f9712b1e57e42eebb86b99f53d3eac591e61837
loicquessette <l.quessette@gmail.com>
2025-02-05 15:26:33 +0100

    fix : menu déroulant ne marchais pas après avoir cliqué sur un lien

--- GreenScoreWebsite/assets/js/base.js
+++ GreenScoreWebsite/assets/js/base.js
@@ -1,39 +1,41 @@
-// Sélection des éléments nécessaires
-const burgerButton = document.getElementById('burger-button');
-const mobileMenu = document.getElementById('mobile-menu');
-const profileButton = document.getElementById('profile-button');
-const profileMenu = document.getElementById('profile-menu');
+document.addEventListener('turbo:load', () => {
 
-// Fonction pour gérer le menu burger
-burgerButton?.addEventListener('click', (event) => {
-    event.stopPropagation(); // Empêche la propagation pour éviter la fermeture immédiate
-    mobileMenu.classList.toggle('hidden'); // Bascule l'état du menu mobile
+    const burgerButton = document.getElementById('burger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const profileButton = document.getElementById('profile-button');
+    const profileMenu = document.getElementById('profile-menu');
 
-    // Assure que le menu profil est fermé
-    profileMenu?.classList.add('hidden');
-});
+    // Fonction pour gérer le menu burger
+    burgerButton?.addEventListener('click', (event) => {
+        event.stopPropagation();
+        mobileMenu.classList.toggle('hidden');
 
-// Fonction pour gérer le menu profil
-profileButton?.addEventListener('click', (event) => {
-    event.stopPropagation(); // Empêche la propagation pour éviter la fermeture immédiate
-    profileMenu.classList.toggle('hidden'); // Bascule l'état du menu profil
+        // Assure que le menu profil est fermé
+        profileMenu?.classList.add('hidden');
+    });
 
-    // Assure que le menu burger est fermé
-    mobileMenu?.classList.add('hidden');
-});
+    // Fonction pour gérer le menu profil
+    profileButton?.addEventListener('click', (event) => {
+        event.stopPropagation();
+        profileMenu.classList.toggle('hidden');
 
-// Gestion des clics en dehors des menus pour les fermer
-document.addEventListener('click', () => {
-    mobileMenu?.classList.add('hidden');
-    profileMenu?.classList.add('hidden');
-});
+        // Assure que le menu burger est fermé
+        mobileMenu?.classList.add('hidden');
+    });
 
-// Gestion des clics sur les liens des menus
-const menuLinks = document.querySelectorAll('#mobile-menu a, #profile-menu a');
-menuLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.stopPropagation(); // Empêche la propagation pour ne pas déclencher la fermeture immédiate
-        mobileMenu?.classList.add('hidden'); // Ferme le menu mobile
-        profileMenu?.classList.add('hidden'); // Ferme le menu profil
+    // Gestion des clics en dehors des menus pour les fermer
+    document.addEventListener('click', () => {
+        mobileMenu?.classList.add('hidden');
+        profileMenu?.classList.add('hidden');
+    });
+
+    // Gestion des clics sur les liens des menus
+    const menuLinks = document.querySelectorAll('#mobile-menu a, #profile-menu a');
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.stopPropagation();
+            mobileMenu?.classList.add('hidden'); // Ferme le menu mobile
+            profileMenu?.classList.add('hidden'); // Ferme le menu profil
+        });
     });
 });
